8cefddbe6fe01c8ff829aa79e630199d3d05bbf8
Mnnxe <napatminnie@gmail.com>
2025-11-19 02:24:38 +0700

    AddCard and read CardInSet

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 #include <string.h>
 #include <ctype.h>
 
-void AddSet() //create new set file (title.txt) and add title to set_list
+struct Flashcard
+{
+    char word[100];
+    char definition[100];
+
+};
+
+int AddSet() //create new set file (title.txt) and add title to set_list
 {
     char file_name[50];
     char title[50];
@@ -26,9 +33,54 @@ void AddSet() //create new set file (title.txt) and add title to set_list
     fclose(fp);
     fclose(master);
 }
-void SetList() //print all list
+
+int AddCard(char *title)
 {
-    char str[100];
+    char w[100];
+    char d[100];
+    char file_name[50];
+
+    sprintf(file_name,"%s.txt",title);
+    printf("Enter word (or enter 0 to go back): ");
+    gets(w);
+
+    printf("Enter definition (or enter 0 to go back): ");
+    gets(d);
+
+    FILE *fp = fopen(file_name,"w");
+    if(fp == NULL)
+    {
+        printf("<<Error open file>>");
+        return 101;
+    }
+
+    fprintf(fp,"%s:%s\n",w,d);
+
+    fclose(fp);
+}
+
+int CardInSet(char *title)
+{
+    char file_name[50];
+    char str[200];
+    sprintf(file_name,"%s.txt",title);
+
+    FILE *fp = fopen(file_name,"r");
+    if(fp == NULL)
+    {
+        printf("<<Error open file>>");
+        return 101;
+    }
+
+    while(fgets(str,100,fp)!= NULL)
+    {
+        printf("%s\n",str);
+    }
+    fclose(fp);
+}
+int SetList() //print all list
+{
+    char str[200];
     FILE *fp = fopen("set_list.txt","r");
     if(fp == NULL)
     {
@@ -43,7 +95,11 @@ void SetList() //print all list
 }
 int main()
 {
+    char title[] = "Alphabet";
     AddSet();
     SetList();
+    AddCard(title);
+    CardInSet(title);
+
     return 0;
 }
--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 
 int main()
 {
-    printf("Project");
+    printf("Flashcard Project");
+
     return 0;
 }
