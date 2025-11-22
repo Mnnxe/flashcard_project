e66cfa0f3b254627ace222e58482546da10ec9d1
pim <pimpichcha.sae@student.mahidol.edu>
2025-11-22 16:06:40 +0700

    Add files via upload

---
+++ character.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+int main()
+{
+    printf("\033[32;5m");
+printf("\t       *************************\n ");
+printf("\t      **-----------------------**      \n ");
+printf("\t     ***-----------------------***    \n");
+printf("\t    **--------#---------#--------**   \n");
+printf("\t    **--------#---------#---------**    \n");
+printf("\t   ***----------------------------***   \n");
+printf("\t  **--------------------------------**    \n");
+printf("\t  **------------vvvvvvv-------------**\n");
+printf("\t  **------------^^^^^^^-------------**\n");
+printf("\t  **--------------------------------**\n");
+printf("\t   **********************************  \n");
+
+printf(" \33[0m\n");
+printf(" \33[91m\n\n");
+printf("     A               \n");
+printf("    T^T                \n");
+printf("    T T               \n");
+printf("    T T     ^^^^^^^^^           \n");
+printf("    T T     | __ __ |           \n");
+printf("    T T    H|  U  U |H           \n");
+printf("    T T    H|    |  |H           \n");
+printf("  #######   |_______|                 \n");
+printf("     H      ===   ===        \n");
+printf("    (H)>>>>///     \\\\\\<<o            \n");
+printf("     H      II     II   V            \n");
+printf("            II     II   V      \n");
+printf("            II=====II  (O)   \n");
+printf("            /  / \\  \\           \n");
+printf("           /  /   \\  \\       \n");
+printf("       <wwwwwww)  (wwwwwww>         \n");
+
+printf("\033[94m");
+printf("                  \n");
+printf("              @ \n");
+printf("              /\\    \n");
+printf("             /  \\     \n");
+printf("            /  O \\    \n");
+printf("           /  O*O \\   \n");
+printf("          /    O   \\  \n");
+printf("      <<<<---------->>>>           \n");
+printf("     O     | __?__ |           \n");
+printf("    O*O   q|  y  y |p          \n");
+printf("     O     |    l  |           \n");
+printf("   #####   |_______|                 \n");
+printf("     H      ===   ===        \n");
+printf("    (H)>>>>///     \\\\\\<<o            \n");
+printf("     H      II     II   V            \n");
+printf("     H      II     II   V      \n");
+printf("     H      II=====II  (O)   \n");
+printf("     H      /  / \\  \\           \n");
+printf("     H     /  /   \\  \\       \n");
+printf("     H <<<<<<<)  (>>>>>>>>         \n");
+
+printf(" \33[0m\n");
+
+
+}
